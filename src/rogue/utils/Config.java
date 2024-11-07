dcd6c5a1c6513f7f3e1b2da7b38e0b8d0b2b4f72
kevinsilva <kevinsilva.j@gmail.com>
2024-11-07 12:55:31 +0000

    feat: adds config class

---
+++ src/rogue/utils/Config.java
@@ -0,0 +1,14 @@
+package pt.upskill.projeto1.rogue.utils;
+
+public class Config {
+    final private String ROOM_DIRECTORY = "rooms/";
+    final private String[] ROOM_FILES = {"room0.txt"};
+
+    public String getROOM_DIRECTORY() {
+        return ROOM_DIRECTORY;
+    }
+
+    public String[] getROOM_FILES() {
+        return ROOM_FILES;
+    }
+}
