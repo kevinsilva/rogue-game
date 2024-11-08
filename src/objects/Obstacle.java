78f7aacf7d7e559a2a0d9e28f2b4992af44f0ca5
kevinsilva <kevinsilva.j@gmail.com>
2024-11-08 11:26:38 +0000

    feat: adds Obstacle

---
+++ src/objects/Obstacle.java
@@ -0,0 +1,19 @@
+package pt.upskill.projeto1.objects;
+
+import pt.upskill.projeto1.rogue.utils.Position;
+
+public class Obstacle extends GameObject {
+    public Obstacle(Position position) {
+        super(position);
+    }
+
+    @Override
+    public String getName() {
+        return "Obstacle";
+    }
+
+    @Override
+    public void react(GameObject otherObject) {
+        System.out.println("Obstacle");
+    }
+}
