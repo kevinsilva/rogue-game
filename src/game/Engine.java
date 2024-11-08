267a84272bdb55fe287c3fd180dad6faa7522d5a
kevinsilva <kevinsilva.j@gmail.com>
2024-11-08 11:25:35 +0000

    chore: removes comments from Engine

--- src/game/Engine.java
+++ src/game/Engine.java
@@ -1,43 +1,22 @@
 package pt.upskill.projeto1.game;
 
 import pt.upskill.projeto1.gui.ImageMatrixGUI;
-import pt.upskill.projeto1.gui.ImageTile;
-import pt.upskill.projeto1.objects.Floor;
 import pt.upskill.projeto1.objects.Hero;
-import pt.upskill.projeto1.objects.Room;
 import pt.upskill.projeto1.rogue.utils.Direction;
 import pt.upskill.projeto1.rogue.utils.Position;
 import pt.upskill.projeto1.rogue.utils.Vector2D;
 
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Engine {
-
+    ImageMatrixGUI gui = ImageMatrixGUI.getInstance();
+    Hero hero = Hero.getInstance();
+    RoomManager rm = new RoomManager(hero);
 
     public void init(){
-        ImageMatrixGUI gui = ImageMatrixGUI.getInstance();
-
-        /*List<ImageTile> tiles = new ArrayList<>();
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
-                tiles.add(new Floor(new Position(i, j)));
-            }
-        }*/
-        Hero hero = Hero.getInstance();
-        //tiles.add(hero);
-
-        RoomManager rm = new RoomManager(hero);
-        Room current = rm.getCurrentRoom();
-        current.addHero(hero);
-
-        current.getTiles().forEach(item -> System.out.println(item.getPosition()));
-        System.out.println(current.getTileByPosition(1, 0).getName());
-
+        rm.getCurrentRoom().addHero(hero);
         gui.setEngine(this);
-        //gui.newImages(tiles);
-        gui.newImages(current.getTiles());
+        gui.newImages(rm.getCurrentRoom().getTiles());
         gui.go();
 
         gui.setStatus("O jogo começou!");
@@ -49,22 +28,25 @@ public class Engine {
 
     public void notify(int keyPressed){
         Hero hero = Hero.getInstance();
+        Position position = hero.getPosition();
         if (keyPressed == KeyEvent.VK_DOWN){
-            hero.move(Direction.DOWN.asVector());
-            System.out.println("User pressed down key!");
+            Vector2D direction = Direction.DOWN.asVector();
+            if(rm.getCurrentRoom().isTileMoveable(position.plus(direction))) hero.move(direction);
         }
         if (keyPressed == KeyEvent.VK_UP){
-            hero.move(Direction.UP.asVector());
-            System.out.println("User pressed up key!");
+            Vector2D direction = Direction.UP.asVector();
+            if(rm.getCurrentRoom().isTileMoveable(position.plus(direction))) hero.move(direction);
         }
         if (keyPressed == KeyEvent.VK_LEFT){
-            hero.move(Direction.LEFT.asVector());
-            System.out.println("User pressed left key!");
+            Vector2D direction = Direction.LEFT.asVector();
+            if(rm.getCurrentRoom().isTileMoveable(position.plus(direction))) hero.move(direction);
         }
         if (keyPressed == KeyEvent.VK_RIGHT){
-            hero.move(Direction.RIGHT.asVector());
-            System.out.println("User pressed right key!");
+            Vector2D direction = Direction.RIGHT.asVector();
+            if(rm.getCurrentRoom().isTileMoveable(position.plus(direction))) hero.move(direction);
         }
+        if (keyPressed == KeyEvent.VK_M) gui.newImages(rm.getNextRoom().getTiles());
+        if (keyPressed == KeyEvent.VK_N) gui.newImages(rm.getPreviousRoom().getTiles());
     }
 
     public static void main(String[] args){
