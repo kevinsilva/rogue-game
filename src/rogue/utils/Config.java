a9dea7b3d28901a09ceaabb5e10b0347d7de934a
kevinsilva <kevinsilva.j@gmail.com>
2024-11-08 11:25:11 +0000

    chore: adds initial position to config

--- src/rogue/utils/Config.java
+++ src/rogue/utils/Config.java
@@ -2,7 +2,8 @@ package pt.upskill.projeto1.rogue.utils;
 
 public class Config {
     final private String ROOM_DIRECTORY = "rooms/";
-    final private String[] ROOM_FILES = {"room0.txt"};
+    final private String[] ROOM_FILES = {"room0.txt", "room1.txt", "room2.txt"};
+    final private Position INITIAL_POSITION = new Position(4, 0);
 
     public String getROOM_DIRECTORY() {
         return ROOM_DIRECTORY;
@@ -10,5 +11,9 @@ public class Config {
 
     public String[] getROOM_FILES() {
         return ROOM_FILES;
+    }
+
+    public Position getINITIAL_POSITION() {
+        return INITIAL_POSITION;
     }
 }
