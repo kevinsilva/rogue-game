30f843cb8ac91200d517e994da1ede7ae4b5b1c1
kevinsilva <kevinsilva.j@gmail.com>
2024-11-08 11:25:48 +0000

    feat: adds GameManager

---
+++ src/game/GameManager.java
@@ -0,0 +1,17 @@
+package pt.upskill.projeto1.game;
+
+public class GameManager {
+    private int score;
+
+    public GameManager() {
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void updateScore() {
+
+    }
+}
