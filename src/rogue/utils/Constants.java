d147cfa87a1d62e14166770a507c4d3a4ed4d1c2
kevinsilva <kevinsilva.j@gmail.com>
2024-11-09 19:04:59 +0000

    chore: changes to constants

--- src/rogue/utils/Constants.java
+++ src/rogue/utils/Constants.java
@@ -1,6 +1,6 @@
 package pt.upskill.projeto1.rogue.utils;
 
-public class Config {
+public class Constants {
     final private String ROOM_DIRECTORY = "rooms/";
     final private String[] ROOM_FILES = {"room0.txt", "room1.txt", "room2.txt"};
     final private Position INITIAL_POSITION = new Position(4, 0);
