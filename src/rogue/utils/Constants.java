bc3d71f2abcb60a2a33d7df6b66f044f49b33e5a
kevinsilva <kevinsilva.j@gmail.com>
2024-11-11 21:07:36 +0000

    chore: updates constants

--- src/rogue/utils/Constants.java
+++ src/rogue/utils/Constants.java
@@ -1,19 +1,24 @@
 package pt.upskill.projeto1.rogue.utils;
 
 public class Constants {
-    final private String ROOM_DIRECTORY = "rooms/";
-    final private String[] ROOM_FILES = {"room0.txt", "room1.txt", "room2.txt"};
+    final private String[] ROOM_FILENAMES = {"room0.txt", "room1.txt", "room2.txt"};
     final private Position INITIAL_POSITION = new Position(4, 0);
 
     public String getROOM_DIRECTORY() {
+        String ROOM_DIRECTORY = "rooms/";
         return ROOM_DIRECTORY;
     }
 
-    public String[] getROOM_FILES() {
-        return ROOM_FILES;
+    public String[] getROOM_FILENAMES() {
+        return ROOM_FILENAMES;
     }
 
     public Position getINITIAL_POSITION() {
         return INITIAL_POSITION;
+    }
+
+    public int getSTATUS_BAR_WIDTH() {
+        int STATUS_BAR_WIDTH = 10;
+        return STATUS_BAR_WIDTH;
     }
 }
