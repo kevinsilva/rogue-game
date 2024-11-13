d0d352a07237c3b08504aac9e986db6469db370d
kevinsilva <kevinsilva.j@gmail.com>
2024-11-13 16:02:16 +0000

    feat: adds EmptyInventory

---
+++ src/objects/EmptyInventory.java
@@ -0,0 +1,14 @@
+package pt.upskill.projeto1.objects;
+
+import pt.upskill.projeto1.rogue.utils.Position;
+
+public class EmptyInventory extends Inventory {
+    public EmptyInventory(Position position) {
+        super(position);
+    }
+
+    @Override
+    public String getName() {
+        return "Empty Inventory";
+    }
+}
