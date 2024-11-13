c67cee1f2ef7447dc6d3256f93c280f20aa98376
kevinsilva <kevinsilva.j@gmail.com>
2024-11-13 16:02:59 +0000

    feat: adds validateImpact

--- src/objects/Fire.java
+++ src/objects/Fire.java
@@ -1,13 +1,15 @@
 package pt.upskill.projeto1.objects;
 
+import pt.upskill.projeto1.game.RoomManager;
+import pt.upskill.projeto1.game.StatusManager;
 import pt.upskill.projeto1.gui.FireTile;
 import pt.upskill.projeto1.rogue.utils.Position;
 
-public class Fire implements FireTile {
-    private Position position;
-
-    public Fire(Position position) {
-        this.position = position;
+public class Fire extends GameObject implements FireTile {
+    RoomManager roomManager;
+    public Fire(Position position, RoomManager roomManager) {
+        super(position);
+        this.roomManager = roomManager;
     }
 
     @Override
@@ -16,17 +18,30 @@ public class Fire implements FireTile {
     }
 
     @Override
-    public Position getPosition() {
-        return position;
-    }
-
-    @Override
     public boolean validateImpact() {
-        return false;
+        int nextX = this.getPosition().getX() + 1;
+        int nextY = this.getPosition().getY();
+        GameObject otherObject = roomManager.getCurrentRoom().getGameObject(nextX, nextY);
+
+        if (!(otherObject.isWalkable())) {
+            FireOld fireold = new FireOld(this.getPosition());
+
+            roomManager.getCurrentRoom().removeGameObject(this);
+            roomManager.getCurrentRoom().addGameObject(fireold);
+            roomManager.updateGUI();
+            roomManager.getCurrentRoom().removeGameObjectAfterMs(fireold, 150);
+
+            return false;
+        }
+        return true;
     }
 
     @Override
-    public void setPosition(Position position) {
-        this.position = position;
+    public void react(GameObject otherObject, RoomManager roomManager, StatusManager statusManager) {
+    }
+
+    @Override
+    public boolean isWalkable() {
+        return false;
     }
 }
